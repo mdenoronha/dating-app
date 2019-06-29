14f934a731cc340c4975c35f62a6c3d92d500005
Ubuntu <ubuntu@ip-172-31-32-41.ec2.internal>
2019-06-29 14:08:28 +0000

    Add profiles_profile to gcd_formula

--- home/views.py
+++ home/views.py
@@ -54,10 +54,11 @@ def index(request):
     
 def preregister(request):
     from django.db.models.expressions import RawSQL
+    
     gcd_formula = "6371 * acos(cos(radians(%s)) * \
-        cos(radians(cityLat)) \
-        * cos(radians(cityLong) - radians(%s)) + \
-        sin(radians(%s)) * sin(radians(cityLat)))"
+        cos(radians(profiles_profile.cityLat)) \
+        * cos(radians(profiles_profile.cityLong) - radians(%s)) + \
+        sin(radians(%s)) * sin(radians(profiles_profile.cityLat)))"
     distance_raw_sql = RawSQL(
         gcd_formula,
         (30, 20, 30)
