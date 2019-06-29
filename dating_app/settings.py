1034eddb7b1afb0865af6672aa2415d9fad58aaf
Ubuntu <ubuntu@ip-172-31-32-41.ec2.internal>
2019-06-29 13:56:52 +0000

    Make debug False explicit

--- dating_app/settings.py
+++ dating_app/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('secret_key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('HOSTNAME'), "5a824725c9bd45ef92e402d580741815.vfs.cloud9.us-east-1.amazonaws.com"]
 
