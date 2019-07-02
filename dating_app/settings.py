17332b63cf5e6954747cb231013f52085eb7b398
Ubuntu <ubuntu@ip-172-31-32-41.ec2.internal>
2019-07-02 15:51:50 +0000

    Change timezone

--- dating_app/settings.py
+++ dating_app/settings.py
@@ -18,11 +18,13 @@ import os
 if os.environ.get('DEVELOPMENT'):
     development = True
     import env
+    
 else:
     development = False
     
-
 USE_TZ = True
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,7 +149,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
