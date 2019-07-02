98218a920ad7b5d7a24477b34947379d619f7cc6
Ubuntu <ubuntu@ip-172-31-32-41.ec2.internal>
2019-07-02 15:48:22 +0000

    Add verification page

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from profiles.views import logout, login, register, user_profile, create_profile, validate_username, member_profile
+from profiles.views import logout, login, register, user_profile, create_profile, validate_username, member_profile, delete, verification_message
 from profiles import url_reset
 
 urlpatterns = [
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^register/', register, name='register'),
     url(r'^create-profile/', create_profile, name='create_profile'),
     url(r'^edit/', user_profile, name="user_profile"),
+    url(r'^delete/', delete, name="delete"),
+    url(r'^verification-message/', verification_message, name="verification_message"),
     url(r'^member/(?P<id>\d+)', member_profile, name='member_profile'),
     url(r'^ajax/validate_username/$', validate_username, name='validate_username'),
     url(r'^password-reset/', include(url_reset))
