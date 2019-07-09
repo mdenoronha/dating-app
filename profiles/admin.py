8053106e1639e0c2496ed082366e680cb7ced894
Matthew de Noronha <matthewdenoronha@gmail.com>
2019-07-09 01:04:58 +0000

    Update email sender address

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,42 +2,33 @@ from django.contrib import admin
 from .models import Profile, ProfileImage
 from django.core.mail import send_mass_mail
 
-# # Register your models here.
-# class OrderLineAdminInline(admin.TabularInline):
-#     model = Profile, ProfileImage
-    
-# class OrderAdmin(admin.ModelAdmin):
-#     inlines = (OrderLineAdminInline, )
-
-
 def verify(modeladmin, request, queryset):
     queryset.update(is_verified='APPROVED')
 
     emailtuple = ()
     for value in queryset:
-        for query in queryset:
-            if isinstance(query, Profile):
-                emailtuple = emailtuple + (('Your account has been approved', 'yay!', 'matthewdenoronha@gmail.com', [value.user.email]),)
-            else: 
-                emailtuple = emailtuple + (('Your profile photo has been approved', 'The following image has been approved: "%s"> '% query.image.url, 'matthewdenoronha@gmail.com', [value.user.email]),)
+        if isinstance(value, Profile):
+            emailtuple += (('Your account has been approved', 'We have good news! Your account has been approved, you can now start dating!', 'worlds.best.dating.app@gmail.com', [value.user.email]),)
+        else: 
+            emailtuple += (('Your profile photo has been approved', 'Thank you for submitting your profile photos. The following image has been approved: "%s"> '% value.image.url, 'worlds.best.dating.app@gmail.com', [value.user.email]),)
                 
-    
-             
     send_mass_mail(emailtuple)
-    
         
 def reject(modeladmin, request, queryset):
-    queryset.update(is_verified='NOT APPROVED')
     emailtuple = ()
     
     for value in queryset:
-        for query in queryset:
-            if isinstance(query, Profile):
-                emailtuple = emailtuple + (('Your account has not been approved', 'boo!', 'matthewdenoronha@gmail.com', [value.user.email]),)
-            else: 
-                emailtuple = emailtuple + (('Your profile photo has been rejected', 'The following image has been rejected: "%s"> '% query.image.url, 'matthewdenoronha@gmail.com', [value.user.email]),)
+        if isinstance(value, Profile):
+            emailtuple = emailtuple + (('Your account has not been approved', 'Unfortunately your bio does not follow our community guidelines. Please update it by editing your profile.', 'worlds.best.dating.app@gmail.com', [value.user.email]),)
+        else: 
+            emailtuple = emailtuple + (('Your profile photo has been rejected', 'One of your profile photos do not follow our community guidelines and has been deleted', 'worlds.best.dating.app@gmail.com', [value.user.email]),)
      
     send_mass_mail(emailtuple)
+    
+    if isinstance(queryset.first(), Profile):
+        queryset.update(is_verified='TO BE APPROVED')
+    else: 
+        queryset.delete()
     
 verify.short_description = "Mark selected as verified"
 reject.short_description = "Mark selected as rejected"
@@ -47,7 +38,6 @@ class ProfileAdmin(admin.ModelAdmin):
     list_filter = ('is_verified', )
     readonly_fields = ('id',)
     actions = (verify, reject)
-    # list_display_links = ('id', 'title')
     list_per_page = 30
     
 admin.site.register(Profile, ProfileAdmin)
