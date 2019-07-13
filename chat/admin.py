26de87276b3062ef60375d63782ce34e14aadd93
Matthew de Noronha <matthewdenoronha@gmail.com>
2019-07-13 18:25:16 +0000

    Add reject to admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,20 +1,25 @@
 from django.contrib import admin
-from .models import Messages, Conversations, Winks
+from .models import Messages, Conversations, Winks, Reject
 
 # Register your models here.
 
-class MessagesAdmin(admin.ModelAdmin):
+class MessageAdmin(admin.ModelAdmin):
     model = Messages
     list_display = ('sender', 'receiver', 'message_content', 'created_on', 'is_read')
     
-class ConversationsAdmin(admin.ModelAdmin):
+class ConversationAdmin(admin.ModelAdmin):
     model = Conversations
     list_display = ('id', )
     
-class WinksAdmin(admin.ModelAdmin):
+class WinkAdmin(admin.ModelAdmin):
     model = Winks
     list_display = ('sender', 'receiver',  'created_on', 'is_read')
+    
+class RejectAdmin(admin.ModelAdmin):
+    model = Reject
+    list_display = ('sender', 'receiver',  'created_on', 'is_read')
 
-admin.site.register(Messages, MessagesAdmin)
-admin.site.register(Winks, WinksAdmin)
-admin.site.register(Conversations, ConversationsAdmin)
+admin.site.register(Messages, MessageAdmin)
+admin.site.register(Winks, WinkAdmin)
+admin.site.register(Conversations, ConversationAdmin)
+admin.site.register(Reject, RejectAdmin)
